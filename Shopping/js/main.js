7cfa06033600c325ed5851e4a9142b17df0e5d99
SURAJ <mynisks@gmail.com>
2018-08-27 08:05:51 +0530

    3 pages completed

---
+++ Shopping/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('#more').click(function() {
+        $('#more-options').show();
+    })
+
+    $('#close').click(function() {
+        $('#more-options').hide();
+    })
+})
